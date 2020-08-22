bca6ab513932b2ba3fd4e876df43ea9efe83b5c3
Luis Leopardi <leopardi.luis@gmail.com>
2020-08-22 10:52:28 -0500

    chore(coffes): add timeout and wrapper response

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { WrapResponseInterceptor } from './common/interceptors/wrap-response.interceptor';
+import { TimeoutInterceptor } from './common/interceptors/timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +13,10 @@ async function bootstrap() {
       whitelist: true,
       transform: true,
     }),
+  );
+  app.useGlobalInterceptors(
+    new WrapResponseInterceptor(),
+    new TimeoutInterceptor(),
   );
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
